33bea69c49c45a7633a9dd91f92a966aa2b8264f
shubham <vickygaikwad41996@gmail.com>
2024-03-05 15:53:49 +0530

    SPRT001 modified data.tf db_subnet_group.tf output.tf

--- data.tf
+++ data.tf
@@ -1,11 +1,11 @@
 data "aws_vpc" "vpc_available" {
+
   filter {
     name   = "tag:Name"
     values = ["nike_vpc"]
   }
 }
-data "aws_subnet_ids" "available_db_subnet" {
-  vpc_id = data.aws_vpc.vpc_available.id
+data "aws_subnets" "available_db_subnet" {
   filter {
     name   = "tag:Name"
     values = ["nike_database*"]
